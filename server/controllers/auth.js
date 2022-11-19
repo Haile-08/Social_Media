a62323c270abf10765753154b1ff86ec2ec2dc13
Haile-08 <haile4cmd@gmail.com>
2022-11-19 15:41:19 +0300

    Added the authorization and auth

---
+++ server/controllers/auth.js
@@ -0,0 +1,60 @@
+import bcrypt from "bcrypt";
+import pkg from "jsonwebtoken";
+import User from "../models/User.js";
+
+const { Jwt } = pkg;
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+    const salt = await bcrypt.genSalt();
+    const passwordhash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordhash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 1000),
+      impressions: Math.floor(Math.random() * 1000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* Login */
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ msg: "User doesn't exisit" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid password" });
+    }
+
+    const token = Jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
+
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -33,6 +36,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.use("/auth/register", upload.single("picture"), register);
+
+/* Routes */
+
+app.use("/auth", authRoutes);
 /* MONGOOSE */
 
 const PORT = process.env.PORT || 6001;
